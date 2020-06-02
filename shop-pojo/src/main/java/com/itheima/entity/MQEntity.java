fb79efb119125518463a6f1309054fbfe6ed6a63
yanyupingsheng395 <395351080@qq.com>
2020-06-02 16:15:53 +0800

    增加详解

--- shop-pojo/src/main/java/com/itheima/entity/MQEntity.java
+++ shop-pojo/src/main/java/com/itheima/entity/MQEntity.java
@@ -3,12 +3,17 @@ package com.itheima.entity;
 import java.math.BigDecimal;
 
 public class MQEntity {
-
+    //订单id
     private Long orderId;
+    //订单金额
     private Long couponId;
+    //用户id
     private Long userId;
+    //用户余额
     private BigDecimal userMoney;
+    //商品ID
     private Long goodsId;
+    //购买数量
     private Integer goodsNum;
 
     public Long getOrderId() {
